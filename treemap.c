f9cd4044ec04d1f23b2ad900e1f6a54742ac183c
Replit user <>
2023-04-24 15:23:24 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -42,12 +42,11 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
   arbol->lower_than = lower_than;
   return arbol;
     //new->lower_than = lower_than;
-    //return NULL;
 }
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-  
+
 }
 
 TreeNode * minimum(TreeNode * x){
