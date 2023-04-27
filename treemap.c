2a777afddb34b2878d378832626ed1fc8a37dd56
Replit user <>
2023-04-27 16:04:02 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -132,13 +132,13 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 
 
 Pair * upperBound(TreeMap * tree, void* key) {
-  tree->current = tree->root;
+  /*tree->current = tree->root;
   if(tree->current->pair->key == key)return tree->current->pair;
-  /*while(true)
+  while(true)
     {
       if
-    }
-    return NULL;*/
+    }*/
+    return NULL;
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
